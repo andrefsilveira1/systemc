58eda00627389ee46ebe500656016ef0ddc71ee2
André Freitas <freitasandre38@gmail.com>
2024-04-14 14:51:00 -0300

    feat: create module register

---
+++ registers/design.cpp
@@ -0,0 +1,26 @@
+#include <systemc.h>
+
+SC_MODULE(Register) {
+    sc_in<bool> clock;  
+    sc_in<bool> reset;  
+    sc_in<bool> enable; // operações de escrita
+    sc_in<sc_uint<32>> data_in;  
+    sc_out<sc_uint<32>> data_out;
+
+    sc_uint<32> reg_data;
+
+    SC_CTOR(Register) : reg_data(0) {
+        SC_METHOD(registerProcess);
+        sensitive << clock.pos();
+    }
+
+    void registerProcess() {
+        if (reset.read()) {
+            reg_data = 0; 
+        } else if (enable.read()) {
+            reg_data = data_in.read();
+        }
+       
+        data_out.write(reg_data);
+    }
+};
